bccdb1bbdf51bd91c31f688e80fa932142fde02c
Muhammad Atiq <atiqghata70@gmail.com>
2023-10-03 02:17:17 +0500

    fix provider

--- src/Providers/CRMServiceProvider.php
+++ src/Providers/CRMServiceProvider.php
@@ -20,7 +20,7 @@ class CRMServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             if (! class_exists('CreateSbxSettingsTable')) {
                 $this->publishes([
-                    __DIR__ . '/../database/migrations/create_sbx_settings_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_sbx_settings_table.php'),
+                    __DIR__ . '/../database/migrations/2023_09_28_174935_create_sbx_settings_table.php' => $this->app->databasePath('database/migrations/' . date('Y_m_d_His', time()) . '_create_sbx_settings_table.php'),
                 ], 'migrations');
             }
         }
